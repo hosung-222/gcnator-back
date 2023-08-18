95d2c82be5f75a6bad24cce9f9479f86fa4e0fe0
hosung-222 <gik11kr@naver.com>
2023-08-19 02:12:53 +0900

    feat 유저 등록시 모든 가중치가 0으로 초기화 되는 함수 생성
    모든 카테고리에 대해서 로그인시 가중치가 0으로 초기화 된다.

--- src/main/java/com/example/demo/src/user/UserService.java
+++ src/main/java/com/example/demo/src/user/UserService.java
@@ -1,21 +1,38 @@
 package com.example.demo.src.user;
 
+import com.example.demo.src.question.CategoryRepository;
+import com.example.demo.src.question.UserWeightRepository;
+import com.example.demo.src.question.entity.Category;
+import com.example.demo.src.question.entity.UserWeight;
 import com.example.demo.src.user.dto.LoginDto;
 import com.example.demo.src.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+    private final UserWeightRepository userWeightRepository;
+    private final CategoryRepository categoryRepository;
 
     @Transactional
     public void login(LoginDto loginDto) {
         User user = new User();
         user.setUser(loginDto);
+
+        // 회원가입시 모든 카테고리에 대하여 가중치 다 0 으로 셋팅해야함
+        List<Category> categories = categoryRepository.findAll();
+        for (Category c :categories){
+            UserWeight userWeight = new UserWeight();
+            userWeight.setUser(user);
+            userWeight.setWeight(0);
+            userWeight.setCategory(c);
+        }
         userRepository.save(user);
     }
 }
